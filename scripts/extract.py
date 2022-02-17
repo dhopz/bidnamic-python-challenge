5b105ec59dd81c8a4067744152d902b782e7896e
dhopz <david_houpapa@hotmail.com>
2022-02-17 17:24:42 +0000

    setting up sqlalchemy and database

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,8 +4,10 @@ from pprint import pprint
 
 # Settings
 base_path = os.path.dirname(os.path.abspath("__file__"))
-adgroups_path = f"{base_path}/data/raw/adgroups.csv"
-raw_path = f"{base_path}/data/raw_adgroups.csv"
+data_path = f"{base_path}/data/raw/search_terms.csv"
+raw_path = f"{base_path}/data/new_search_terms.csv"
+
+
 
 # Create extraction method - may need to amend fields
 def extract_raw_data():
@@ -13,7 +15,7 @@ def extract_raw_data():
     Save data from the source
     """
 
-    with open(adgroups_path, mode="r") as csv_file:
+    with open(data_path, mode="r") as csv_file:
         reader = csv.DictReader(csv_file)    
         # Print the first row
         row = next(reader)
@@ -33,6 +35,6 @@ def extract_raw_data():
 # Main function called inside the execute.py script
 def main():
     print("[Extract] Start")
-    print(f"[Extract] Saving data from '{adgroups_path}' to '{raw_path}'")
+    print(f"[Extract] Saving data from '{data_path}' to '{raw_path}'")
     extract_raw_data()
     print(f"[Extract] End")
---
+++ scripts/transform.py
@@ -0,0 +1,5 @@
+import os
+import csv
+from datetime import datetime
+
+base_path = os.path.dirname(os.path.abspath("__file__"))
